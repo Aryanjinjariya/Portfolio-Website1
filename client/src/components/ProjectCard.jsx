0f00bccdc29a268bb5dde04586cda06e91ab258c
Aryan Jinjariya <aryan.jinjariya@example.com>
2026-05-15 17:36:42 +0530

    Updated CORS for Vercel frontend

--- client/src/components/ProjectCard.jsx
+++ client/src/components/ProjectCard.jsx
@@ -1,16 +1,25 @@
 import { useNavigate } from 'react-router-dom'
+
 const ProjectCard = ({ project }) => {
 	const navigate = useNavigate()
 
-	const BASE_URL = 'http://localhost:3001'
+	const BASE_URL = 'https://portfolio-website1-ejud.onrender.com'
 
 	return (
 		<div
 			className='glass project-card'
 			onClick={() => navigate(`/project/${project._id}`)}
 		>
-			<img src={`${BASE_URL}${project.image}`} alt={project.title} />
+			<img
+				src={`${BASE_URL}${project.image}`}
+				alt={project.title}
+				onError={e => {
+					e.target.src = 'https://via.placeholder.com/400x250?text=No+Image'
+				}}
+			/>
+
 			<h3>{project.title}</h3>
+
 			<p>{project.description}</p>
 		</div>
 	)
