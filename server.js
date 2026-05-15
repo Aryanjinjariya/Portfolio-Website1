975597e18e0efb390672bad93fc7d63dfcfa351e
Aryan Jinjariya <aryan.jinjariya@example.com>
2026-05-15 13:04:02 +0530

    Updated portfolio project

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ connectDB()
 app.use(express.json())
 app.use(
 	cors({
-		origin: 'https://portfolio-website1-zeta-eight.vercel.app/',
+		origin: 'https://portfolio-website1-zeta-eight.vercel.app',
 		credentials: true
 	})
 )
