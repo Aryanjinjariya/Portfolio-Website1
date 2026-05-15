c2d0694bf1fadb9f4394dbe98f60e4a4e3bf3d51
Aryan Jinjariya <aryan.jinjariya@example.com>
2026-05-15 13:14:16 +0530

    Updated portfolio project

--- server.js
+++ server.js
@@ -5,10 +5,8 @@ require('dotenv').config()
 
 const app = express()
 
-// Connect Database
 connectDB()
 
-// Middleware
 app.use(express.json())
 
 // CORS Configuration
@@ -35,25 +33,20 @@ app.use(
 	})
 )
 
-// Handle preflight requests
 app.options('*', cors())
 
-// Static folder
 app.use('/upload', express.static('upload'))
 
-// Routes
 app.use('/api/register-admin', require('./route/api/register-Admin'))
 app.use('/api/login-admin', require('./route/api/login-Admin'))
 app.use('/api/contact', require('./route/api/contact'))
 app.use('/api/project', require('./route/api/project'))
 app.use('/api/stats-admin', require('./route/api/stats-admin'))
 
-// Test Route
 app.get('/', (req, res) => {
 	res.send('API is running')
 })
 
-// Server
 const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
